db59d6be527790b9e89b70269841eff43d07524d
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2020-10-09 16:53:37 +0200

    Animation and debugging
    -made the issue from the previous commit work. I originally thought you could just type the name of the tag instead of needing an actual existing object for the "getComputedCSS" function.
    
    -added all necessary animations and made some simple keyframes for each one
    -added time based functionality to the animation
    -added a section where I initially tested timed functions(the p tag id'd as "act")
    -added a debug area which hold buttons to manually switch between animations and check certain variables while the page is running
    -changed the title of the page to "Clock"
    
    -made several functions in js:
         -changed the displayName function into getBodyAttr, allowing more general attribute value acquisition than just for animationName
         -added switchAnimation and animationSwitcher, the first switches to an animation whilst the other switches between animations based on the current time
           -changed clock display formatting so that when the time on whichever value is lesser than 10 it displays a "0" before the number
           -added goAct to test timed function checking and execution

--- time.js
+++ time.js
@@ -6,14 +6,80 @@ function display_c(){
 
 function display_ct(){
     var x = new Date();
-    var formatX=x.getHours()+":"+x.getMinutes()+":"+x.getSeconds();
+    //var formatX=x.getHours()+":"+x.getMinutes()+":"+x.getSeconds();
+
+    if(x.getHours()<10)formatX="0"+x.getHours()+":";
+    else formatX=x.getHours()+":";
+
+    if(x.getMinutes()<10)formatX+="0"+x.getMinutes()+":";
+    else formatX+=x.getMinutes()+":";
+
+    if(x.getSeconds()<10)formatX+="0"+x.getSeconds();
+    else formatX+=x.getSeconds();
+
     document.getElementById('Clock').innerHTML = formatX;
+    goAct(x);
+    animationSwitcher(x);
     display_c();
 }
 
-function displayAName(){
+function getBodyAttr(attr){
 
-    Name=document.styleSheets.;
+    elem=document.getElementsByTagName('body')[0];
 
-    alert(Name[0].value);
+    let currentAnimation=window.getComputedStyle(elem).getPropertyValue(attr);
+
+    return currentAnimation;
 }
+
+function displayAnimationName(){
+    name=getBodyAttr('animation-name');
+    alert(name);
+}
+
+function displayCurrentBackgroundColor(){
+    color=getBodyAttr('background-color');
+    alert(color);
+}
+
+function switchAnimation(toThis){
+    bod=document.getElementsByTagName('body')[0];
+    bod.style.animationName=toThis;
+}
+
+function animationSwitcher(cuTime){
+    sunriseTime=new Date();
+    dayTime=new Date();
+    sunsetTime=new Date();
+    nightTime=new Date();
+    sunriseTime.setHours(16,39,0);
+    dayTime.setHours(16,40,0);
+    sunsetTime.setHours(16,38,0);
+    nightTime.setHours(16,39,0);
+
+    if(cuTime>=sunriseTime && cuTime < dayTime) switchAnimation('sunRise');
+
+    if(cuTime==dayTime) switchAnimation('dayTime');
+
+    if(cuTime==sunsetTime) switchAnimation('sunSet');
+
+    if(cuTime==nightTime) switchAnimation('nightTime');
+
+
+}
+
+
+function goAct(curTime){
+    actTimeStart=new Date();
+    actTimeEnd=new Date();
+    actTimeStart.setHours(15,15,0);
+    actTimeEnd.setHours(15,15,30);
+    
+    if(curTime>=actTimeStart && curTime < actTimeEnd){
+        document.getElementById('act').innerHTML="The time to act is NOW!";
+    }else{
+        document.getElementById('act').innerHTML="Now is not the time to act...";
+    }
+}
+
+
